a090863b6f6872f1e96b837fcfe34fb0c0f8c342
k.aasan <k.aasan@sportradar.com>
2025-11-22 18:18:12 +0100

    fix: e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run start:standalone",
+    command: "npm run start",
     url: BASE_URL,
     reuseExistingServer: !env.CI,
     timeout: 10_000,
